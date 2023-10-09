0d863a3d6292a4138e6988bd5b07d762d17ed0eb
Jamie Till <114414595+JamieT1ll@users.noreply.github.com>
2023-10-09 12:51:02 +0100

    Clarity Fix

--- menu.sh
+++ menu.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
 #Menu For The Program
-PS3='Please enter your choice: '
+PS3='Please enter the number of your choice: '
 options=("1. Create File" "2. Edit File" "3. Delete File" "4. Check Logs" "Exit")
 select opt in "${options[@]}"
 do
     case $opt in
-        "Option 1")
+        "1")
             echo "Create File"
             ;;
-        "Option 2")
+        "2")
             echo "Edit File"
             ;;
-        "Option 3")
+        "3")
             echo "Delete File"
             ;;
-        "Option 4")
-            echo "Logs"
+        "4) "
+        echo "Logs"
             ;;
         "Exit")
             break
             ;;
-        *) echo "invalid option $REPLY";;
+        *) 
+        echo "invalid option $REPLY";;
     esac
 done
