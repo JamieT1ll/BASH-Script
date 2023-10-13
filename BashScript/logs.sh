d34c6046b7c629f32411e96db9c0004b288f7f72
Jamie Till <114414595+JamieT1ll@users.noreply.github.com>
2023-10-13 10:57:22 +0100

    bashing

--- BashScript/logs.sh
+++ BashScript/logs.sh
@@ -1,2 +1,29 @@
 #!/bin/bash
 
+PS3='What would you like to do with the logs?: '
+option=("1. View Logs" "2. Revert Changes" "e) Exit")
+select opt in "${option[@]}"
+do
+    case $opt in
+        "1")
+         #Displays Logs
+            echo "Opening Logs..."
+            ./create.sh
+            ;;
+        "2")
+           #Reverts Changes
+            echo "What changes would you like to revert?"
+            ./edit.sh
+            ;;
+        
+        
+        "e")
+        echo "Exiting the menu"
+        exit
+        ;;
+
+        #Input validation for incorrect inputs
+        *) 
+        echo "invalid option: $REPLY, please enter a Number from the menu for your choice.";;
+    esac
+done
