b0430e85619a164aa2b86272382623d585e25d24
Jamie Till <114414595+JamieT1ll@users.noreply.github.com>
2023-10-15 02:58:37 +0100

    logs

--- BashScript/editFile.sh
+++ BashScript/editFile.sh
@@ -1,88 +1,104 @@
 #!/bin/bash
 
-echo "which of the following repositories contains the file you wish to edit?: "
-    ls -I menu
-    read rName
+echo "Which of the following repositories contains the file you wish to edit?: "
+ls -I menu
+read rName
 
-    while true; do
-        if [ -d "$rName/mainBranch" ]; then
-            cd "$rName/mainBranch"
-            echo "Files in the repository:"
-            ls
-            echo -e "\n"
+while true; do
+    if [ -d "$rName/mainBranch" ]; then
+        cd "$rName/mainBranch"
+        echo "Files in the repository:"
+        ls
+        echo -e "\n"
 
-            while true; do
-                echo "Would you like to edit the file on the main branch?"
-                echo "['y' for yes 'n' for no]: "
-                read choice
+        while true; do
+            echo "Would you like to edit the file on the main branch?"
+            echo "['y' for yes, 'n' for no]: "
+            read choice
 
-                case "$choice" in
-                [yY])
-                    echo "Enter the name of the file you wish to edit: "
-                    read fileName
+            case "$choice" in
+            [yY])
+                echo "Enter the name of the file you wish to edit: "
+                read fileName
+
+                if [ -e "$fileName" ]; then
+                    cp "$fileName" "../backup/$fileName"  # Store a copy in the 'backup' folder
                     nano "$fileName"
                     break 2
-                    ;;
-                [nN])
-                    while true; do
-                        echo "Does the branch already exist? (y/n): "
-                        read branchExistsChoice
+                else
+                    echo "File '$fileName' does not exist in the main branch."
+                fi
+                ;;
+            [nN])
+                while true; do
+                    echo "Does the branch already exist? (y/n): "
+                    read branchExistsChoice
 
-                        case "$branchExistsChoice" in
-                        [yY])
-                            echo "List of existing branches:"
-                            ls -d */  # List all directories (branches) in the current directory
-                            echo "Enter the name of the branch you want to switch to: "
-                            read branchName
+                    case "$branchExistsChoice" in
+                    [yY])
+                        echo "List of existing branches:"
+                        ls -d */  # List all directories (branches) in the current directory
+                        echo "Enter the name of the branch you want to switch to: "
+                        read branchName
 
-                            if [ -d "$branchName" ]; then
-                                cd "$branchName"  # Corrected path
-				ls
-				echo "Enter the name of the file you wish to edit: "
-				read fileName
-				nano $fileName
+                        if [ -d "$branchName" ]; then
+                            cd "$branchName"
+                            ls
+                            echo "Enter the name of the file you wish to edit: "
+                            read fileName
+
+                            if [ -e "$fileName" ]; then
+                                cp "$fileName" "../backup/$fileName"  # Store a copy in the 'backup' folder
+                                nano "$fileName"
                                 break 3  # Exit outer while loop as well
                             else
-                                echo "Branch '$branchName' does not exist."
+                                echo "File '$fileName' does not exist in branch '$branchName'."
                             fi
-                            ;;
-                        [nN])
-                            echo "Enter new branch name: "
-                            read branchName
+                        else
+                            echo "Branch '$branchName' does not exist."
+                        fi
+                        ;;
+                    [nN])
+                        echo "Enter new branch name: "
+                        read branchName
 
-                            if [ -d "$branchName" ]; then
-                                echo "Branch '$branchName' already exists."
-                            else
-                                mkdir "$branchName"
+                        if [ -d "$branchName" ]; then
+                            echo "Branch '$branchName' already exists."
+                        else
+                            mkdir "$branchName"
 
-                                echo "Enter the name of the file you wish to edit: "
-                                read fileName
-                                cp "$fileName" "$branchName"
-				cd $branchName
-				nano $fileName
+                            echo "Enter the name of the file you wish to edit: "
+                            read fileName
+
+                            if [ -e "$fileName" ]; then
+                                cp "$fileName" "../backup/$fileName"  # Store a copy in the 'backup' folder
+                                cd "$branchName"
+                                nano "$fileName"
                                 break 2
+                            else
+                                echo "File '$fileName' does not exist in the main branch."
                             fi
-                            ;;
-                        *)
-                            echo "Invalid input. Please enter 'y' for yes or 'n' for no."
-                            ;;
-                        esac
-                    done
-                    ;;
-                    
-                *)
-                    echo "Invalid input. Please enter 'y' for yes or 'n' for no."
-                    ;;
-                esac
-            done
+                        fi
+                        ;;
+                    *)
+                        echo "Invalid input. Please enter 'y' for yes or 'n' for no."
+                        ;;
+                    esac
+                done
+                ;;
+            *)
+                echo "Invalid input. Please enter 'y' for yes or 'n' for no."
+                ;;
+            esac
+        done
 
-            cd ~/assignment1
-            break
-        else
-            echo "Invalid repository name. Please check if the repository exists."
-            echo "Enter the repository containing the file: "
-            echo "which of the following repositories contains the file you wish to edit?: "
-            ls -I menu
-            read rName
-        fi
-    done
+        cd ~/assignment1
+        break
+    else
+        echo "Invalid repository name. Please check if the repository exists."
+        echo "Enter the repository containing the file: "
+        echo "Which of the following repositories contains the file you wish to edit?: "
+        ls -I menu
+        read rName
+    fi
+done
--- BashScript/logs.sh
+++ BashScript/logs.sh
@@ -1,29 +1,43 @@
 #!/bin/bash
 
 PS3='What would you like to do with the logs?: '
-option=("1. View Logs" "2. Revert Changes" "e) Exit")
-select opt in "${option[@]}"
+options=("1. View Logs" "2. Revert Changes" "3. Clear Logs" "e) Exit")
+select opt in "${options[@]}"
 do
     case $opt in
         "1")
-         #Displays Logs
+            # Displays Logs
             echo "Opening Logs..."
             ./create.sh
             ;;
-        "2")
-           #Reverts Changes
-            echo "What changes would you like to revert?"
-            ./edit.sh
-            ;;
-        
-        
-        "e")
-        echo "Exiting the menu"
-        exit
-        ;;
 
-        #Input validation for incorrect inputs
-        *) 
-        echo "invalid option: $REPLY, please enter a Number from the menu for your choice.";;
+        "2")
+            # Reverts Changes
+            echo "Enter the name of the file you want to revert changes for: "
+            read fileName
+            if [ -e "backup/$fileName" ]; then
+                cp "backup/$fileName" .  # Restore the backup file
+                echo "Changes reverted for $fileName."
+            else
+                echo "Backup file for $fileName not found. No changes reverted."
+            fi
+            ;;
+
+        "3")
+            # Clears Logs
+            echo "Clearing Logs..."
+            rm -f logs/*  # Remove all files in the 'logs' directory
+            echo "Logs cleared."
+            ;;
+
+        "e")
+            echo "Exiting the menu."
+            exit
+            ;;
+
+        # Input validation for incorrect inputs
+        *)
+            echo "Invalid option: $REPLY, please enter a number from the menu for your choice."
+            ;;
     esac
 done
