2471d0c7a4f0d7430e75372ee562b6098356e0d7
Jamie Till <114414595+JamieT1ll@users.noreply.github.com>
2023-10-16 01:18:16 +0100

    yuh

--- BashScript/checklogs.sh
+++ BashScript/checklogs.sh
@@ -12,17 +12,24 @@ do
             ;;
 
         "2")
-            # Reverts Changes
-            echo "Enter the name of the file you want to revert changes for: "
-            read fileName
-            if [ -e "backup/$fileName" ]; then
-                cp "$fileName" "backup/$fileName.$(date +'%Y%m%d%H%M%S')"  # Create a new backup file
-                cp "backup/$fileName" .  # Restore the backup file
-                echo "Changes reverted for $fileName."
-            else
-                echo "Backup file for $fileName not found. No changes reverted."
-            fi
-            ;;
+            "2")
+             # Reverts Changes
+             echo "Enter the name of the file you want to revert changes for: "
+             read fileName
+             if [ -e "backup/$fileName" ]; then
+             if [ -e "$fileName" ]; then
+              mv "$fileName" "temp_$fileName"  # Temporarily rename the current file
+               cp "backup/$fileName" "$fileName"  # Copy the backup file as the new current file
+               mv "temp_$fileName" "backup/$fileName"  # Move the current file to the backup folder
+             echo "Changes reverted for $fileName."
+        else
+             echo "Current file $fileName not found. No changes reverted."
+        fi
+    else
+        echo "Backup file for $fileName not found. No changes reverted."
+    fi
+    ;;
+
 
          "3")
           # Clears Logs
